516c8491ba6a2062de314a0eb3f840b5d8120342
GedMarc <ged_marc@hotmail.com>
2018-08-10 13:30:12 +0200

    Plugin POM updates for JRE10, and maven dependency classification bug fix

--- src/main/java/com/jwebmp/plugins/ngslimscroll/NgSlimScrollPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/ngslimscroll/NgSlimScrollPageConfigurator.java
@@ -24,6 +24,8 @@ import com.jwebmp.core.plugins.PluginInformation;
 import com.jwebmp.core.plugins.jquery.JQueryPageConfigurator;
 import com.jwebmp.core.services.IPageConfigurator;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Marc Magon
  * @since 08 Jun 2017
@@ -58,6 +60,7 @@ public class NgSlimScrollPageConfigurator
 		component.addAttribute("data-slim-scroll", null);
 	}
 
+	@NotNull
 	@Override
 	public Page configure(Page page)
 	{
