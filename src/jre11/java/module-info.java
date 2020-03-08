7b7b95323c0cdabafabcf4732137743adaa822f8
GedMarc <ged_marc@hotmial.com>
2020-03-08 16:59:48 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -14,7 +14,6 @@ module com.jwebmp.plugins.ngslimscroll {
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.ngslimscroll.NgSlimScrollPageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.ngslimscroll.NgSlimScrollModule;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.ngslimscroll.implementations.NgSlimScrollExclusionsModule;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.ngslimscroll.implementations.NgSlimScrollExclusionsModule;
 
 	opens com.jwebmp.plugins.ngslimscroll to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
--- src/main/java/com/jwebmp/plugins/ngslimscroll/implementations/NgSlimScrollExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/ngslimscroll/implementations/NgSlimScrollExclusionsModule.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.ngslimscroll.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,18 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class NgSlimScrollExclusionsModule
-		implements IGuiceScanModuleExclusions<NgSlimScrollExclusionsModule>,
-				           IGuiceScanJarExclusions<NgSlimScrollExclusionsModule>
+		implements IGuiceScanModuleExclusions<NgSlimScrollExclusionsModule>
 {
-
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-ng-slim-scroll-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
